dfcebf6e8b8ef3ee8969dd2dd4051c94067139e3
HotDoguito <miguelsonmaia@gmail.com>
2025-12-12 23:19:23 -0300

    Update dump.c

--- dump.c
+++ dump.c
@@ -6,10 +6,13 @@ void menu();
 int main() {
     int opt;
     
-    char tabuleiro[10][10];
+    char tabuleiro[10][10][2];
+    char linha = '1', coluna = 'A';
     for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
-        tabuleiro[i][j] = '.';  // '.' representa água
+        for (int k = 0; k < 2; k++){
+        tabuleiro[i][j] = 
+            }
     }
 }
 
@@ -90,6 +93,7 @@ void menu() {
     printf("3 - INTRUÇÕES\n");
     printf("4 - SAIR\n");
 }
+
 
 
 
