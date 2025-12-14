f835925105adbed9a39572639c08ff1357861b6b
HotDoguito <miguelsonmaia@gmail.com>
2025-12-14 18:55:12 -0300

    Update funcsalvar.c

--- testes/funcsalvar.c
+++ testes/funcsalvar.c
@@ -7,3 +7,11 @@ void salvarJogo(Jogador j1, Jogador j2, int turno) {
     fwrite(&turno, sizeof(int), 1, f);
 
     fclose(f);
+
+
+typedef struct {
+    char tabuleiro[N][N];     // Onde ficam seus navios
+    char ataques[N][N];       // Onde registram ataques no inimigo
+    int acertos;
+    int erros;
+} Jogador;
